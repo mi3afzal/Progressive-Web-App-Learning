1c09e8710f5e800f2fd9079f937b32499adad74e
Muhammad Irfan Afzal <mi3afzal@gmail.com>
2018-08-05 17:21:01 +0500

    lecture 2 part 1 complete. fetching data from server.

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
 
 console.log('Hello World');
+var server_url = '/';
 
 if ('serviceWorker' in navigator) {
 	navigator.serviceWorker.register('/sw.js').then(function (registration) {
@@ -11,4 +12,15 @@ if ('serviceWorker' in navigator) {
 	});
 }
 
-
+// take example user data from
+// https://jsonplaceholder.typicode.com/users 
+function getData(){
+	console.log('Get data function called.');
+	fetch(server_url+'users.json').then(function(responce){
+		return responce.json();
+	}).then(function(users){
+		console.log(users);
+	}).catch(function(error){
+		console.log('Not Working. ', error);
+	})
+}
