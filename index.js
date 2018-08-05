29731c9288e6d00d3f73d1dffa1401c13ef64f19
Muhammad Irfan Afzal <mi3afzal@gmail.com>
2018-08-05 14:31:11 +0500

    Saylani Bootcamp. Lecture 1

---
+++ index.js
@@ -0,0 +1,14 @@
+
+console.log('Hello World');
+
+if ('serviceWorker' in navigator) {
+	navigator.serviceWorker.register('/sw.js').then(function (registration) {
+		// Registration was successful
+		console.log('ServiceWorker registration successful with scope: ', registration.scope);
+	}).catch(function (err) {
+		// registration failed :(
+		console.log('ServiceWorker registration failed: ', err);
+	});
+}
+
+
